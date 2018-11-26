c43a52bef7cf169af872801f159890eed4c40ada
Santiago M. Mola <santi@mola.io>
2018-11-26 17:01:23 +0100

    tree: more tests, fix type hints
    Signed-off-by: Santiago M. Mola <santi@mola.io>

--- badcode/tree.py
+++ badcode/tree.py
@@ -19,13 +19,13 @@ class Tree(Generic[K]):
         return self._key
 
     @property
-    def children(self) -> Tuple['Tree[K]']:
+    def children(self) -> Sequence['Tree[K]']:
         return self._children
 
     def with_children(self, children: Iterable['Tree[K]']) -> 'Tree[K]':
         return self.__class__(key=self.key, children=tuple(children))
 
-    def __eq__(self, other: 'Tree[K]') -> bool:
+    def __eq__(self, other):
         if other is None:
             return False
         if self.key != other.key:
@@ -45,6 +45,8 @@ class Tree(Generic[K]):
         return self._len
 
     def __repr__(self) -> str:
+        if len(self.children) == 0:
+            return '%s(key=%s)' % (self.__class__.__name__, self.key)
         return '%s(key=%s, children=%s)' % (self.__class__.__name__, self.key, self.children)
 
     def __getstate__(self):
@@ -67,5 +69,4 @@ class Tree(Generic[K]):
     def __copy__(self) -> 'Tree[K]':
         return self.__class__(
             key=self.key,
-            children=(c.__copy__() for c in self.children)
-        )
+            children=(c.__copy__() for c in self.children))
--- test/test_tree.py
+++ test/test_tree.py
@@ -19,3 +19,46 @@ def test_tree_eq_hash():
     assert hash(a) == hash(b)
     assert a != c
     assert hash(a) != hash(c)
+
+def test_tree_eq_none():
+    assert Tree(key=1) != None
+
+def test_tree_len():
+    t = Tree(key=1)
+    assert len(t) == 1
+
+    t = Tree(key=1, children=[
+        Tree(key=2)
+    ])
+    assert len(t) == 2
+
+    t = Tree(key=1, children=[
+        Tree(key=2),
+        Tree(key=3, children=[
+            Tree(key=4),
+        ]),
+    ])
+    assert len(t) == 4
+
+def test_repr():
+    t = Tree(key=1)
+    assert t.__repr__() == 'Tree(key=1)'
+
+    t = Tree(key=1, children=[
+        Tree(key=2),
+        Tree(key=3)
+    ])
+    assert t.__repr__() == 'Tree(key=1, children=(Tree(key=2), Tree(key=3)))'
+
+def test_state():
+    t = Tree(key=1, children=[
+        Tree(key=2),
+        Tree(key=3)
+    ])
+
+    state = t.__getstate__()
+    assert list(state.keys()) == ['key', 'children']
+
+    t2 = Tree(key=0)
+    t2.__setstate__(state)
+    assert t == t2
